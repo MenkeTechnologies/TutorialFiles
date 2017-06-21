84d83dcc336275098d45e2b4e06dde986c21a995
MenkeTechnologies <linux.dev25@gmail.com>
2017-06-21 10:48:29 -0400

    update

--- shell/menkeTech.sh
+++ shell/menkeTech.sh
@@ -5,9 +5,9 @@ tput civis
 
 trap 'tput cnorm;exit' INT
 while 'true'; do
-		clear
-		printf "\e[1m"
-        figlet -f cosmic Menke Technologies
-        print "\e[0m"
-		sleep 1000
+    clear
+    printf "\e[1m"
+    figlet -f cosmic Menke Technologies
+    printf "\e[0m"
+    sleep 1000
 done

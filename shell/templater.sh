bc26c38f962eca40f907362ba3eef2016fcb5844
Jacob Menke <jamenk@email.wm.edu>
2017-06-22 16:06:02 -0400

    update

--- shell/templater.sh
+++ shell/templater.sh
@@ -2,10 +2,8 @@
 #created by JACOBMENKE at Mon Jun 12 17:33:50 EDT 2017
 
 fileName="$1"
-
 echo '#!/usr/bin/env bash' > "$fileName"
-
 printf '#created by JACOBMENKE at ' >> "$fileName"
-printf "$(date)" >> "$fileName"
+printf "%s" "$(date)" >> "$fileName"
+printf " with %s\n" "$SHELL" >> "$fileName"
 
-printf "\n" >> "$fileName"

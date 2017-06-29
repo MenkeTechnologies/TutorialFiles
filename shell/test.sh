73c3c2cc6c1590190cfbe954e97912e4c50d6592
Jacob Menke <jamenk@email.wm.edu>
2017-06-29 08:47:31 -0400

    update

--- shell/test.sh
+++ shell/test.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
-#created by JACOBMENKE at Wed Jun 28 21:15:32 EDT 2017 with /usr/local/bin/zsh
+#created by JACOBMENKE at Wed Jun 28 21:45:18 EDT 2017 with /usr/local/bin/zsh
 
 
 
-
-
-dogs(){
-
-
-}
-
-
-
+echo "dogs are cool in my  mount"
 
 

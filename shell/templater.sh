5c1143d07f0faa3ebbf15a5c6d0e378afff3f7c4
MenkeTechnologies <linux.dev25@gmail.com>
2017-06-30 15:07:26 -0400

    update

--- shell/templater.sh
+++ shell/templater.sh
@@ -2,8 +2,18 @@
 #created by JACOBMENKE at Mon Jun 12 17:33:50 EDT 2017
 
 fileName="$1"
-echo '#!/usr/bin/env bash' > "$fileName"
-printf '#created by JACOBMENKE at ' >> "$fileName"
-printf "%s" "$(date)" >> "$fileName"
-printf " with %s\n" "$SHELL" >> "$fileName"
-echo  >> "$fileName"
+
+firstString=$(cat<<EOM
+#!/usr/bin/env bash
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: `date`
+#####   Purpose: 
+#####   Notes: 
+#}}}***********************************************************
+
+EOM
+)
+
+echo "$firstString" > "$fileName"
--- shell/test.sh
+++ shell/test.sh
@@ -4,6 +4,12 @@
 echo "my pwd is `pwd`"
 
 
+a=1 b=$((2))
+c=$a+$b d=$(($a+$b))
+echo "$a + $b = $c \t(plus sign as string literal)"
+echo "$a + $b = $d \t(plus sign as arithmetic addition)"
+
+
 
 
 

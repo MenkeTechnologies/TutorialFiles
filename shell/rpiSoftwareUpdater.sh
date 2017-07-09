a8a526595520781e924d091ee257df57ad2b1ee2
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-08 20:58:45 -0400

    update

---
+++ shell/rpiSoftwareUpdater.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Sat Jul  8 20:57:59 EDT 2017
+#####   Purpose: bash script to update software on the RPI itself
+#####   Notes: 
+#}}}***********************************************************
+
+prettyPrint "Updating Pathogen Plugins"
+#update pathogen plugins
+for pluginRepo in ~/.vim/bundle/*; do
+	printf "%s: " "$(basename "$pluginRepo")"
+	git -C "$pluginRepo" pull
+done
+
+prettyPrint "Updating OhMyZsh"
+#upgrade_oh_my_zsh
+
+prettyPrint "Updating OhMyZsh Plugins"
+
+for zshPlugin in ~/.oh-my-zsh/custom/plugins/*; do
+	printf "%s: " "$(basename "$zshPlugin")"
+	git -C "$zshPlugin" pull
+done
+
+prettyPrint "Updating OhMyZsh Themes"
+
+for zshPlugin in ~/.oh-my-zsh/custom/themes/*; do
+	printf "%s: " "$(basename "$zshPlugin")"
+	git -C "$zshPlugin" pull
+done
+

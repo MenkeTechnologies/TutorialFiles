50f4eb70e7485dbfb70795eb2df028a61d348fbb
Jacob Menke <jamenk@email.wm.edu>
2017-07-08 20:57:04 -0400

    update

--- shell/test.sh
+++ shell/test.sh
@@ -1,34 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -i
 #created by JACOBMENKE at Wed Jun 28 21:45:18 EDT 2017 with /usr/local/bin/zsh
 
-echo "my pwd is `pwd`"
-
-echo "the number of args is $#"
-
-a=1 b=$((2+5))
-c=$a+$b d=$(($a+$b))
-echo "$a + $b = $c \t(plus sign as string literal)"
-echo "$a + $b = $d \t(plus sign as arithmetic addition)"
-
-
-echo "my id is `id`"
-
-echo "the pwd is `pwd`"
-
-#{{{                    MARK:adfasdfsd
-#**************************************************************
 
 
 
-
-#}}}***********************************************************
-
-
-
-
-
-
-#{{{                    MARK:sdfsdf
-#**************************************************************
-
-#}}}***********************************************************

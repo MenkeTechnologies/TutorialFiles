0e04d0d52e1a2eaef3777092b02430e28af415a1
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-10 19:24:09 -0400

    update

--- shell/rpiDownloader.sh
+++ shell/rpiDownloader.sh
@@ -1,4 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash 
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Mon Jul 10 19:23:26 EDT 2017
+#####   Purpose: bash  script to download from RPi to local host
+#####   Notes: 
+#}}}***********************************************************
+
 printf "${BLUE}Downloading $*\n"
 for i in "$@"; do
 	scp -P1234 -r "$i" jacobmenke@localhost:~/Desktop

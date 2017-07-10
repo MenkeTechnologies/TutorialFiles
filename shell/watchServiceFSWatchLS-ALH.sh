0efe6e7c85f420290a12b63e36f719779c7b3d02
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-10 19:34:22 -0400

    update

--- shell/watchServiceFSWatchLS-ALH.sh
+++ shell/watchServiceFSWatchLS-ALH.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-#created by JAKOBMENKE --> Sat Jan 14 18:12:20 EST 2017 
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Mon Jul 10 19:33:52 EDT 2017
+#####   Purpose: bash script to list files on file change
+#####   Notes: 
+#}}}***********************************************************
 DIR_WATCHING="$1"
 while read -d "" event; do
 	clear

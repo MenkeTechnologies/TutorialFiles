3aa509aedad133d9b81e8ae35af839ef29219185
Jacob Menke <jamenk@email.wm.edu>
2017-07-17 13:28:37 -0400

    update

--- shell/macColorLoggers.sh
+++ shell/macColorLoggers.sh
@@ -6,7 +6,12 @@
 #####   Purpose: bash script to monitor log files in color
 #####   Notes: requires globstar option
 #}}}***********************************************************
+
 shopt -s globstar
 
-colortail -k $HOME/.colortailconf -f /var/log/**/*.log /var/log/**/*.out 
+if [[ `uname` = Darwin ]]; then
+    colortail -k $HOME/.colortailconf -f /var/log/**/*.log /var/log/**/*.out 
+else
+    colortail -k /home/pi/.colortailconf -f /var/log/**/*.log /var/log/{dmesg,wtmp,debug,lastlog,messages} /var/log/**/*.err
+fi
 

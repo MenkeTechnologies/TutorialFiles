5c778efcfa95b5f1cf7eb468cd006a497ce074e1
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-21 22:00:41 -0400

    added supported for ccze if available, no more colortail due to cpu usage

--- shell/colorLogger.sh
+++ shell/colorLogger.sh
@@ -9,27 +9,28 @@
 
 shopt -s globstar
 
-set -x
-tailVersion="colortail -k $HOME/.colortailconf"
+#tailVersion="colortail -k $HOME/.colortailconf"
+tailVersion=tail
 
-type colortail 1>/dev/null 2>&1 || {
-    tailVersion=tail
+type ccze 1>/dev/null 2>&1 && {
+    pipeVersion=" | ccze"
 }
 
+
+
 if [[ $(uname) == Darwin ]]; then
-  tailVersion="tail"
-  $tailVersion -f /var/log/**/*.log /var/log/**/*.out /var/log/DiagnosticMessages \
-        /var/log/asl /var/log/cups/* "$HOME"/Library/Logs/**/*.log "$HOME"/Library/Logs/**/*.out \
-        /Library/Logs/**/*.log | ccze
+  echo $tailVersion -f /var/log/**/*.log /var/log/**/*.out /var/log/DiagnosticMessages/*.asl \
+      /var/log/asl/*.asl /var/log/cups/* "$HOME"/Library/Logs/**/*.log "$HOME"/Library/Logs/**/*.out \
+        /Library/Logs/**/*.log $pipeVersion
 else
     #linux
     distroName=$(lsb_release -a | head -1 | awk '{print $3}')
 
     if [[ $distroName == Raspbian ]]; then
-        $tailVersion -f /var/log/**/*.log /var/log/{dmesg,debug,lastlog,messages} /var/log/**/*.err
+        $tailVersion -f /var/log/**/*.log /var/log/{dmesg,debug,lastlog,messages} /var/log/**/*.err $pipeVersion
     else 
         printf "Unsupported distro: $distroName...but trying anyways\n" >&2
-        $tailVersion -f /var/log/**/*.log /var/log/{dmesg,debug,lastlog,messages} /var/log/**/*.err
+        $tailVersion -f /var/log/**/*.log /var/log/{dmesg,debug,lastlog,messages} /var/log/**/*.err $pipeVersion
     fi
 
 fi

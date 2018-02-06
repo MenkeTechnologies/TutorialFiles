10338a809106efae349c2ca5cb4f49afd47052d9
MenkeTechnologies <linux.dev25@gmail.com>
2018-02-05 20:26:36 -0500

    test

--- shell/macOnly/tutorialConfigUpdater.sh
+++ shell/macOnly/tutorialConfigUpdater.sh
@@ -67,7 +67,7 @@ cp -R $HOME/.config/ncmpcpp "$tutorialDir/ncmpcpp-mpd-vis"
 cp -R $HOME/.mpd "$tutorialDir/ncmpcpp-mpd-vis"
 
 boldAndUnderlinedPrint "Copying iterm Colors"
-cp "$HOME/Documents/iterm-jm-colors.itermcolors" "$tutorialDir"
+cp "$HOME/iterm-jm-colors.itermcolors" "$tutorialDir"
 
 boldAndUnderlinedPrint "Copying vim plugins"
 
@@ -143,7 +143,7 @@ cp $HOME/conf.df"$installerDir"
 boldAndUnderlinedPrint "Updating vim plugins list"
 bash "$SCRIPTS/gitRemoteRepoInformation.sh "$HOME/.vim/bundle/"* > "$installerDir/.vimbundle"
 boldAndUnderlinedPrint "Updating zsh plugins list"
-bash "$SCRIPTS/gitRemoteRepoInformation.sh "$HOME/.oh-my-zsh/custom/plugins"* > "$installerDir/.zshplugins"
+bash "$SCRIPTS/gitRemoteRepoInformation.sh" "$HOME/.oh-my-zsh/custom/plugins/"* > "$installerDir/.zshplugins"
 
 cd "$installerDir" || exit 1
 boldAndUnderlinedPrint "Status"

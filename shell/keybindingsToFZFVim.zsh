fbe99277267811c5cdf8fde65d2372a4db4bcfee
MenkeTechnologies <linux.dev25@gmail.com>
2019-12-05 02:16:09 -0500

    zpwr_tempile env

--- shell/keybindingsToFZFVim.zsh
+++ shell/keybindingsToFZFVim.zsh
@@ -12,8 +12,8 @@ else
     cmd=vim
 fi
 
-if [[ -z "$TEMPFILE" ]]; then
-    TEMPFILE="/tmp/.temp$$"
+if [[ -z "$ZPWR_TEMPFILE" ]]; then
+    ZPWR_TEMPFILE="/tmp/.temp$$"
 fi
 {
     $cmd -e -c 'redir! > ~/.temp1 | silent imap | redir END | quitall'
@@ -26,10 +26,10 @@ fi
     cat ~/.temp4
     command rm ~/.temp{1..4}
 
-} | perl -ne 'print if /\S+/' > "$TEMPFILE"
+} | perl -ne 'print if /\S+/' > "$ZPWR_TEMPFILE"
 
 #do not know why have to create tempfile here
-perl -pe 's@^([^#].*)$@$1@g' "$TEMPFILE" | perl -pe 's@(.*) \(:.map\).*@$1@'
+perl -pe 's@^([^#].*)$@$1@g' "$ZPWR_TEMPFILE" | perl -pe 's@(.*) \(:.map\).*@$1@'
 
-command rm "$TEMPFILE"
+command rm "$ZPWR_TEMPFILE"
 
